9a5f29d4c65223a8e6164306d5fcef6a82b35a86
blanketmanatee <ashleyl.welch@gmail.com>
2020-09-17 22:43:57 +0000

    fixed repo re-push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a string using puts
+ * description: print exact quote followed by a new line
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+}
