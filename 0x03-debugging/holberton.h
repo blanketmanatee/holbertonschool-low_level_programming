746e18647d3690fb4aa16363bce6c875b4ff4a5e
blanketmanatee <ashleyl.welch@gmail.com>
2020-09-23 02:17:06 +0000

    initial commit

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
