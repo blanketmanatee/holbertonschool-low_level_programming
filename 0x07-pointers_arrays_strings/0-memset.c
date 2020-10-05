ceaa2ec10fe36d82f0eac08088dbc98e879f1394
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-05 20:31:51 +0000

    betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,12 +4,14 @@
  * _memset - fills memory with a constant byte
  * @b: constant byte
  * @n: bytes being filled
+ * @s: char
  * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *p = s;
+
 	while (n > 0)
 	{
 		*p = b;
