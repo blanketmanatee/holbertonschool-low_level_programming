00bb8933e496cccee4ebeb35bba557acd9ccde93
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-05 23:00:01 +0000

    Betty approved

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,19 +2,16 @@
 
 /**
  * _strchr - locates a character in a string
- * @s:
- * @c:
- * Return:
+ * @s: string
+ * @c: character to be found
+ * Return: NULL or pointer
  */
 
 char *_strchr(char *s, char c)
 {
-do
-{
-
-	if (*s == c)
-		return (s);
-}
-	while(s++);
+	do {
+		if (*s == c)
+			return (s);
+	} while (s++);
 	return (0);
 }
