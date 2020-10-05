1642d0406088c8ac7f5b594e3e51fc12269cf306
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-05 22:01:15 +0000

    betty approved

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,16 @@
 
 /**
  * _memcpy - copies memory area
- * @dest:
- * @src:
- * @n:
+ * @dest: destination
+ * @src: bytes to be copied
+ * @n: amount of bytes
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
