ae0b5a25e11068ce1a983e45977ad934f2403572
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-08 04:17:03 +0000

    betty tested and approved

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: char
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
