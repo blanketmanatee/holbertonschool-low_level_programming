3a549bd92ecc137ed37de0299750e363149fb24e
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-14 02:33:57 +0000

    added comments

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,17 +12,19 @@ char *create_array(unsigned int size, char c)
 
 	char *str;
 	unsigned int i;
-
+/* if size if 0 return NULL */
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
+/* allocate memory for str */
 	str = malloc(size * sizeof(*str));
+/* if str is empty return NULL */
 	if (str == 0)
 	{
 		return (NULL);
 	}
+/* i moves through the size of the array */
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
