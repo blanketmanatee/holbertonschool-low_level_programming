85f69ebba889c819647a3f69a52e0ac7e91ae1d1
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-19 20:43:55 +0000

    betty wanted comments

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - prints name of program
+ * @argc: unused
+ * @argv: name
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	argc = argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
