fc6d1f8ac76adc235ac043249faab7fab98168b5
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-20 01:37:22 +0000

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * description: struct with dog information for project
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _dog_h */
