0d30a2ede589b871c99d6fa23c7ab65ebdfa76b2
blanketmanatee <ashleybordenprice@gmail.com>
2020-10-22 04:55:13 +0000

    initial commit

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,8 @@
+#include "calc.h"
+
+/**
+ * get_op_func - gets func
+ * @s:
+ */
+
+int (*get_op_func(char *s))(int, int)
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,17 @@
+#include "calc.h"
+
+/**
+ * op_add - returns sum of a and b
+ * op_sub - returns difference
+ * op_mul - returns product
+ * op_div - returns result
+ * op_mod - returns remainder of division
+ * @a: int
+ * @b: int
+ */
+
+int op_add(int a, int b)
+	int op_sub(int a, int b)
+	int op_mul(int a, int b)
+int op_div(int a, int b)
+	int op_mod(int a, int b)
