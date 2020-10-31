36a2958395b53c82635eee1de07c703c9bd6868d
blanketmanatee <ashleyl.welch@gmail.com>
2020-10-31 04:49:51 +0000

    and these

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,3 +9,34 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	int cnt = 0;
+	list_t *end, *tmp;
+
+	end = malloc(sizeof(list_t));
+	if (!end)
+		return (NULL);
+
+	if (str)
+	{
+		end->str = strdup(str);
+		while(str[cnt] != '\0')
+			cnt++;
+		end->len = cnt;
+	}
+	else
+	{
+		end->str = NULL;
+		end->len = 0;
+	}
+	end->next = NULL;
+	if (*head)
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = end;
+	}
+	else
+		*head = end;
+	return (end);
+}
