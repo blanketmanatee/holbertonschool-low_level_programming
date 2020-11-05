6ea9398fb0add09300da3ff8117002e67735fbaf
blanketmanatee <ashleyl.welch@gmail.com>
2020-11-05 23:08:12 +0000

    betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,14 @@ size_t print_listint(const listint_t *h)
 
 {
 
-        size_t cnt = 0;
+	int cnt = 0;
 
 	while (h != NULL)
 	{
+		cnt++;
 		printf("[%d]\n", h->n);
 		h = h->next;
-		cnt++;
+		printf("\n");
 	}
 	return (cnt);
 }
