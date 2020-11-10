061e02435b3666db9fd2d4f78a60080a4f5c652b
blanketmanatee <ashleyl.welch@gmail.com>
2020-11-10 02:37:26 +0000

    fixed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,14 +7,14 @@
  * Return: 1 on success -1 on fail
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int a, b;
 
 	if (filename == NULL)
 		return (-1);
 
-	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	a = open(filename,  O_WRONLY | O_APPEND);
 	if (a == -1)
 		return (-1);
 
