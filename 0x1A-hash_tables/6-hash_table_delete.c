a474fe17a5271b1e66e5c1c542778b359c4609be
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-12 01:18:17 -0600

    betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,23 +9,23 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t **array, *node, *tmp;
-    unsigned long int size, i;
+	hash_node_t **array, *node, *tmp;
+	unsigned long int size, i;
 
-    size = ht->size;
-    array = ht->array;
-    for (i = 0; i < size; i++)
-    {
-        node = array[i];
-        while (node)
-        {
-            tmp = node;
-            node = node->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-        }
-    }
-    free(array);
-    free(ht);
+	size = ht->size;
+	array = ht->array;
+	for (i = 0; i < size; i++)
+	{
+		node = array[i];
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(array);
+	free(ht);
 }
