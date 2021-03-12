0586e30f784a0558fa9fcd28cdb46ec61241972d
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-12 00:53:02 -0600

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,24 +10,24 @@
  */
  
  hash_table_t *hash_table_create(unsigned long int size)
- {
-     unsigned long int i = 0;
-     hash_table_t *htable = NULL;
-     hash_node_t **arr = NULL;
+{
+	unsigned long int i = 0;
+	hash_table_t *htable = NULL;
+	hash_node_t **arr = NULL;
 
-     htable = malloc(sizeof(hash_table_t));
-     if (htable == NULL)
-        return (NULL);
+	htable = malloc(sizeof(hash_table_t));
+	if (htable == NULL)
+		return (NULL);
     
-    arr = malloc(sizeof(hash_node_t));
-    if (arr == NULL)
-        return (NULL);
+	arr = malloc(sizeof(hash_node_t));
+	if (arr == NULL)
+		return (NULL);
     
-    for (; i < size; ++i)
-    arr[i] = NULL;
+	for (; i < size; ++i)
+	arr[i] = NULL;
 
-    htable->size = size;
-    htable->array = arr;
-    return(htable);
+	htable->size = size;
+	htable->array = arr;
+	return(htable);
 
- }
+}
