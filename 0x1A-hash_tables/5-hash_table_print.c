05bc2b99e2fb91217bb26d9113e59d9f9b47a7a5
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-12 01:14:46 -0600

    spacing

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,30 +5,30 @@
 /**
  * hash_table_print - prints a hash table
  * @ht: hash table
- * 
+ *
  */
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *tmp = NULL;
+	unsigned long int i = 0;
+	hash_node_t *tmp = NULL;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    while(i < ht->size)
-    {
-        tmp = ht->array[i];
-        while (tmp)
-        {
-            printf("'%s' : '%s'", tmp->key, tmp->value);
-            if (ht->array[i + 1] == NULL)
-                break;
-            printf(", ");
-            tmp = tmp->next;
-        }
-        i++;
-    }
-    printf("}\n");
+	printf("{");
+	while(i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			printf("'%s' : '%s'", tmp->key, tmp->value);
+			if (ht->array[i + 1] == NULL)
+				break;
+			printf(", ");
+			tmp = tmp->next;
+		}
+		i++;
+	}
+	printf("}\n");
 }
