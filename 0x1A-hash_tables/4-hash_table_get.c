1aab38692017d1d5c64fd84bf988464670a2aeab
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-12 01:08:29 -0600

    betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,29 +5,29 @@
 
 /**
  * hash_table_get - retrieves value associated w k
- * @ht:
- * @key:
- * 
+ * @ht: pointer
+ * @key: pointer
+ *
  * Return: value associated with element or NULL
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int idx;
-    hash_node_t *node;
+	unsigned long int idx;
+	hash_node_t *node;
 
-    if (!ht || !key || !*key)
-        return (NULL);
-    idx = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[idx];
+	if (!ht || !key || !*key)
+		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[idx];
 
-    while(node)
-    {
-        if (strcmp(node->key, key) == 0)
-        {
-            return(node->value);
-        }
-        node = node->next;
-    }
-    return (NULL);
+	while(node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return(node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
 }
