f455c64885c25b5a68c63acb42aca08492cf2868
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-12 01:13:03 -0600

    betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,12 +4,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - retrieves value associated w k
- * @ht: pointer
- * @key: pointer
- *
- * Return: value associated with element or NULL
- */
+* hash_table_get - retrieves value associated w k
+* @ht: pointer
+* @key: pointer
+*
+* Return: value associated with element or NULL
+*/
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
