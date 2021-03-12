32c16a06c11836574588cbb1afd1934ed89fad47
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-12 00:15:26 -0600

    betty

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * sum_listint - sum of data in listint_t
- * @head:
+ * @head: pointer to first node
  * Return: 0 if empty
  */
 
@@ -13,9 +13,9 @@ int sum_listint(listint_t *head)
 	if (head == NULL)
 		return (0);
 	while (head)
-    {
+	{
 		sum += head->n;
 		head = head->next;
-    }
+	}
 	return (sum);
 }
