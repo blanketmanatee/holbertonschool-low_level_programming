00e4802639e98691dafa7d2dbe441896271f4bc2
blanketmanatee <ashleyl.welch@gmail.com>
2021-04-20 19:20:07 +0000

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,10 +10,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
-	for (i = 0; i < value; i++)
-		if (array[i] == size)
-			return i;
-	return -1;
+	size_t i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		return (-1);
 
 }
